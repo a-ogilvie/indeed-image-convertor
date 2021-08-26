3cc32adfa4150ec498052657723ecaa669a312da
Alexander Ogilvie <aogilvie@indeed.com>
2021-08-26 11:54:54 +0900

    fix: carry alpha value through from source

--- src/Convertor/index.ts
+++ src/Convertor/index.ts
@@ -2,9 +2,7 @@ import EXPRESSIVE_COLOURS from './ExpressiveColours'
 
 type Colour = Uint8ClampedArray
 
-const memo = new Map<string, Colour>()
-
-const ConvertImage = (image: HTMLImageElement) => {
+const ConvertImage = (image: HTMLImageElement): string => {
   const canvas = document.createElement('canvas')
   canvas.width = image.width
   canvas.height = image.height
@@ -13,6 +11,8 @@ const ConvertImage = (image: HTMLImageElement) => {
     .getContext('2d')
     ?.getImageData(0, 0, canvas.width, canvas.height)
   if (!imageData) return ''
+
+  const memo = new Map<string, Colour>()
 
   for (let i = 0; i < imageData.data.length; i += 4) {
     const inputColour = imageData.data.slice(i, i + 4)
@@ -31,16 +31,13 @@ const ConvertImage = (image: HTMLImageElement) => {
     ] = closestColour
   }
 
-  memo.clear()
-
   canvas.getContext('2d')?.putImageData(imageData, 0, 0)
-  console.log('done!')
   return canvas.toDataURL('image/jpg')
 }
 
 function getClosestColour(inputColour: Colour): Colour {
   let closestDistance = Infinity
-  let closestColour = new Uint8ClampedArray([0, 0, 0, 0])
+  let closestColour = new Uint8ClampedArray([0, 0, 0, 255])
 
   for (const expressiveColour of EXPRESSIVE_COLOURS) {
     const distance = Math.sqrt(
@@ -54,9 +51,10 @@ function getClosestColour(inputColour: Colour): Colour {
       closestColour = expressiveColour
     }
 
-    if (closestDistance === 0) return closestColour
+    if (closestDistance === 0)
+      return new Uint8ClampedArray([...closestColour, inputColour[3]])
   }
 
-  return closestColour
+  return new Uint8ClampedArray([...closestColour, inputColour[3]])
 }
 export default ConvertImage
